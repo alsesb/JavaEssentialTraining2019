cf9ef1b7b088342ab10b3ea61c18b359b8ca9f29
mattb <matt.biro97@gmail.com>
2021-01-25 15:54:37 +0100

    MY Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	    String s = "";
+        try {
+            var sub = s.substring(1);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("Null pointer: " + e.getMessage());
+        }
+        catch (StringIndexOutOfBoundsException ex){
+            ex.printStackTrace();
+            System.out.println("String index out of bound: " + ex.getMessage());
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("This is a general Exception: " + ex.getMessage());
+        }
     }
 }
