3b743330804fb7a2476e386beb1ddc11f02b4c0c
mattb <matt.biro97@gmail.com>
2021-01-25 13:53:06 +0100

    My Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,41 @@ public class Main {
                         "July", "August", "September",
                         "October", "November", "December"};
 
+        for (int i = 0; i < months.length; i++) {
+            System.out.println(months[i]);
+        }
+
+        for (int i = 0; i < 25; i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+
+        for (String month: months){
+            System.out.println(month);
+        }
+
+        for (int i = 0; i < 25; i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+
+        var counter = 0;
+        while(counter < months.length){
+            System.out.println(months[counter]);
+            counter++;
+        }
+
+        for (int i = 0; i < 25; i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+
+        var counter2 = 0;
+        do {
+            System.out.println(months[counter2]);
+            counter2++;
+        }
+        while(counter2 < months.length);
+
     }
 }
