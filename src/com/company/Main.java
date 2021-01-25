0fa9772323c3a5cc5efcfb6ad98b46b0621da8f2
mattb <matt.biro97@gmail.com>
2021-01-25 13:46:30 +0100

    My Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,19 @@ public class Main {
         var now = LocalDateTime.now();
         var monthNumber = now.getMonthValue();
 
+        switch (monthNumber){
+            case 1:
+                System.out.println("The month is January");
+                break;
+            case 2:
+                System.out.println("The month is February");
+                break;
+            case 3:
+                System.out.println("The month is March");
+                break;
+            default:
+                System.out.println("You chose another month.");
+        }
+
     }
 }
