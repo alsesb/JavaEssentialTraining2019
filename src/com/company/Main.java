860432e92174ae7e6edf7372d9e05ce969770bc2
mattb <matt.biro97@gmail.com>
2021-01-25 09:32:46 +0100

    My Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int result = 10 + 12;
-        String answer = "The answer is " + result;
+        int result = 10 * 12;
+        String answer = "The answer is now" + result;
         System.out.println(answer);
     }
 }
