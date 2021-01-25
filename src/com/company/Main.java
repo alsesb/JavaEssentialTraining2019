6b2edb49a0d4a753268c1546fc1139daee8f4185
mattb <matt.biro97@gmail.com>
2021-01-25 11:48:03 +0100

    My Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Enter a numeric value: ");
+        var n1 = scanner.nextDouble();
+        System.out.print("Enter a numeric value: ");
+        var n2 = scanner.nextDouble();
+
+        System.out.println("The answer is: " + (n1 / n2));
+
     }
 }
