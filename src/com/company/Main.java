fc2f2e5402982a7e30454f52c1e0d8be12a1353d
mattb <matt.biro97@gmail.com>
2021-01-25 14:01:38 +0100

    My Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static double getInput(Scanner sc, String prompt){
+        System.out.print(prompt);
+        return sc.nextDouble();
+    }
+
     public static void main(String[] args) {
-
         var sc = new Scanner(System.in);
-
-        System.out.print("Enter a numeric value: ");
-        var d1 = sc.nextDouble();
-
-        System.out.print("Enter a numeric value: ");
-        var d2 = sc.nextDouble();
+        var d1 = getInput(sc, "Enter a numeric value 1: ");
+        var d2 = getInput(sc, "Enter a numeric value 2: ");
 
         double result = d1 / d2;
 
