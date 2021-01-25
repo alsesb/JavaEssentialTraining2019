d6710e161ee52e34f7d7c396b36679714249dd79
mattb <matt.biro97@gmail.com>
2021-01-25 13:42:28 +0100

    My Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var scanner = new Scanner(System.in);
-        System.out.print("Enter a month number (1-12): ");
-        var monthNumber = scanner.nextInt();
+        //var scanner = new Scanner(System.in);
+        //System.out.print("Enter a month number (1-12): ");
+        //var monthNumber = scanner.nextInt();
+        var now = LocalDateTime.now();
+        var monthNumber = now.getMonthValue();
+
+        String message = (monthNumber <= 3) ? "That's in Quarter 1!" : (monthNumber <= 6) ? "That's in Quarter 2" : (monthNumber <= 9) ? "That's in Quarter 3" : "That's in Quarter 4";
+
+        System.out.println(message);
 
     }
 }
